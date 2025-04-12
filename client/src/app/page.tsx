f84e399c37d29bd8a5a7a838e987d15a7674b055
Rohit Jangid <73385164+rohitjangid666@users.noreply.github.com>
2025-04-12 13:02:45 +0530

    Initial commit with client and server

---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <p className='text-center gap-2 mt-4'>
+      <span className='text-4xl font-bold'>Socket.IO Chat</span>
+      <br />
+      <span className='text-sm text-gray-500'>A simple chat application using Socket.IO</span>
+    </p>
+  );
+}
